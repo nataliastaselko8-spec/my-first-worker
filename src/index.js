eac9bdec60aa5f01da4f318a982276c7563e0081
Staselko Natalia <nataliastaselko8@gmail.com>
2026-05-15 16:12:33 +0300

    fix1

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ export default {
     console.log("Method:", request.method);
     console.log("URL:", request.url);
     console.log("ENV:", env.ENV);
-
     return new Response(`ENV = ${env.ENV || "unknown"}`);
   }
 };
