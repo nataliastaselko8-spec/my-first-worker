2b573f16b221da7e6d47894e8deb2af5b4955802
Staselko Natalia <nataliastaselko8@gmail.com>
2026-05-15 16:08:36 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@
 
 export default {
   async fetch(request, env) {
+    console.log("Worker started");
+    console.log("Method:", request.method);
+    console.log("URL:", request.url);
+    console.log("ENV:", env.ENV);
+
     return new Response(`ENV = ${env.ENV || "unknown"}`);
   }
 };
