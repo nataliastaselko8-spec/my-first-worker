44d3f61173df1a467ceea55b35586c2f8f8c5512
Staselko Natalia <nataliastaselko8@gmail.com>
2026-05-15 15:54:42 +0300

    add github actions deploy

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+
 
 export default {
-	async fetch(request, env, ctx) {
-		console.log("🔥 Request received:", request.url)
-		return new Response("Hello World!!!!!");
-	},
+  async fetch(request, env) {
+    return new Response(`ENV = ${env.ENV || "unknown"}`);
+  }
 };
